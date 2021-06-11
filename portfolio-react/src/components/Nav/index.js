5fbda0f04ac122cce570e85b796103321583232f
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-11 10:32:12 -0700

    about compomemt

--- portfolio-react/src/components/Nav/index.js
+++ portfolio-react/src/components/Nav/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 function Nav() {
 
-  return (
-    <header>
-    </header>
-  );
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About me
+        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Nav;
