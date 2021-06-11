b99018c76e5f402fc2d91ca5165eab5e6a92d3d1
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-11 10:26:17 -0700

    about compomemt

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
 
 function App() {
   return (
     <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
