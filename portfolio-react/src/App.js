e43303eaa4862d5c9838922c98c4683a4f4c60dc
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-11 10:29:02 -0700

    about compomemt

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
     </div>
   );
 }
-
 
 export default App;
