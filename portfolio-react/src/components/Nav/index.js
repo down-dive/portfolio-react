d2da580a5346643a15bf00a0bb1db9ef0cf87970
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-11 10:41:08 -0700

    portfolio  compomemt

--- portfolio-react/src/components/Nav/index.js
+++ portfolio-react/src/components/Nav/index.js
@@ -1,28 +1,48 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    contactSelected,
+    currentCategory,
+    setContactSelected,
+  } = props;
 
-    return (
-        <header>
-            <h2>
-                <a href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-    </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
-                            About me
-        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="flex-row px-1">
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
---
+++ portfolio-react/src/components/Portfolip/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Gallery() {
+  return (
+    <section>
+      <h1>PROJECT</h1>
+      <p>DESCRIPTION</p>
+    </section>
+  );
+}
+export default Gallery;
