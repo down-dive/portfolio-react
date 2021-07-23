b662e5ee403610b19960471affc90f1c40338fc0
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-22 21:59:06 -0700

    changing colors

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
 export default function Nav(props) {
-  const classes = useStyles();
   const {
     categories = [],
     contactSelected,
@@ -28,24 +8,25 @@ export default function Nav(props) {
   } = props;
 
   return (
-    <div className="navigation">
-        <input type="checkbox" className="navigation__checkbox" id="navi-toggle"></input>
+    <div classNameName="navigation">
+      <input type="checkbox" className="navigation__checkbox" id="navi-toggle"></input>
 
-        <label for="navi-toggle" class="navigation__button">
-                <span class="navigation__icon">&nbsp;</span>
-            </label>
-            <div class="navigation__background">&nbsp;</div>
-            <nav class="navigation__nav">
-                <ul class="navigation__list">
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>01</span>About Natous</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>02</span>Your benfits</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>03</span>Popular tours</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>04</span>Stories</a></li>
-                    <li class="navigation__item"><a href="#" class="navigation__link"><span>05</span>Book now</a></li>
-                    </ul>
-            </nav>
+      <label for="navi-toggle" class="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+      <div className="navigation__background">&nbsp;</div>
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item"><a href="#about" className="navigation__link" onClick={() => setContactSelected(false)}>
+            About me
+          </a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Contact</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Portfolio</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Resume</a></li>
+        </ul>
+      </nav>
 
-</div>
-  
+    </div>
+
   );
 }
