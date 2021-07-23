c4f74badf9f5fb27ced902ad60c5d613c945885e
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-07-23 15:28:29 -0700

    fixing layout

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -53,48 +53,39 @@ import PortfolioCard from '../PortfolioCard';
 import projects from '../../projects.json';
 
 export default function Portfolio() {
+    // const params = useParams();
+  // const history = useHistory();
+  const [ currentProject, setCurrentProject ] = useState([]);
+  useEffect(() => {
+    // const selectedProject = projects.map(project => project.id === Number(params.projectId));
+    setCurrentProject(projects);
+  }, [])
+  // console.log(params);
+  // console.log(history);
   return (
 <section class="section-tours" id="section-tours">
 <div class="u-center-text u-margin-bottom-big">
     <h2 class="heading-secondary">
-        Most popular tours
+       Projects
     </h2>
 </div>
-
 <div class="row">
-    <div class="col-1-of-3">
-       <div class="card">
-           <div class="card__side card__side--front">
-                <div class="card__picture card__picture--1">
-                    &nbsp;
-                </div>
-                <h4 class="card__heading">
-                    <span class="card__heading-span card__heading-span--1">The Sea Explorer</span>
-                </h4>
-                <div class="card__details">
-                    <ul>
-                        <li>3 day tours</li>
-                        <li>Up to 30 people</li>
-                        <li>2 tour guides</li>
-                        <li>Sleep in cozy hotels</li>
-                        <li>Difficulty: easy</li>
-                    </ul>
-                </div>
-           </div>
-           <div class="card__side card__side--back card__side--back-1">
-                <div class="card__cta">
-                    <div class="card__price-box">
-                        <p class="card__price-only">Only</p>
-                        <p class="card__price-value">$297</p>
-                    </div>
-                    <a href="#popup" class="btn btn--white">Book now!</a>
-                </div>
-            </div>
-       </div>
-    </div>
+<div class='container'>
+      {projects.map((project) => {
+        return <PortfolioCard
+        name={project.name}
+        github={project.github}
+        img={project.img}
+        deployedlink={project.deployedlink}
+        tools={project.tools}
+      />
+      }) }
 
+      {/* <button
+        onClick={() => history.push('/Home')}
+      >Go home</button> */}
+//     </div>
 </div>
-
 <div class="u-center-text u-margin-top-huge">
     <a href="#" class="btn btn--green">Discover all tours</a>
 </div>
--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,48 +1,69 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import './style.css'
+// import { makeStyles } from '@material-ui/core/styles';
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Typography from '@material-ui/core/Typography';
+// import './style.css'
 
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 350,
-  },
-});
-const PortfolioCard = ({ name, github, deployedlink, tools, img}) => {
-  const classes = useStyles();
+// const useStyles = makeStyles({
+//   root: {
+//     maxWidth: 345,
+//   },
+//   media: {
+//     height: 350,
+//   },
+// });
+// const PortfolioCard = ({ name, github, deployedlink, tools, img}) => {
+//   const classes = useStyles();
+//   return (
+//     <Card class="content-container" className={classes.root}>
+//       <CardActionArea >
+//         <CardMedia
+//           className={classes.media}
+//           image={img}
+//           title={name}
+//         />
+//         <CardContent>
+//           <Typography gutterBottom variant="h5" component="h2">
+//             {name}
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary" component="p">
+//           <a href= {github}>Github</a>
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary" component="p">
+//           <a href= {deployedlink}>Deployed link</a>
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary" component="p">
+//             {tools}
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//     </Card>
+//   );
+// }
+
+// export default PortfolioCard
+
+export default function PortfolioCard({ name, github, deployedlink, tools, img }) {
   return (
-    <Card class="content-container" className={classes.root}>
-      <CardActionArea >
-        <CardMedia
-          className={classes.media}
-          image={img}
-          title={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          <a href= {github}>Github</a>
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          <a href= {deployedlink}>Deployed link</a>
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {tools}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div class="col-1-of-3">
+        <div class="card">
+            <h4 class="card__heading">
+              <span class="card__heading-span card__heading-span--1">{name}</span>
+            </h4>
+            <div class="card__details">
+              <ul>
+                <li>{name}</li>
+                <li><a href= {github}>Github</a></li>
+                <li><a href= {deployedlink}>Deployed link</a></li>
+                <li>{tools}</li>
+                <li>Difficulty: easy</li>
+              </ul>
+            </div>
+        </div>
+    </div>
   );
 }
-
-export default PortfolioCard
