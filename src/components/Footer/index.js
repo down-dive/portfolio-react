997ea47642a472d90f7ec5b69f6d8ea868fa8354
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-01 21:59:45 -0700

    icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
   return (
     <footer className='footer'>
       <div className="row">
-        {/* <h3 classNameName='footer-text' >2021 Yevgeniya Terlyuk</h3> */}
         <div className="col-1-of-2" >
           <div className="footer-text">
             <h3>2021 Yevgeniya Terlyuk</h3>
@@ -12,63 +13,22 @@ function Footer() {
         </div>
         <div className="col-1-of-2" >
           <div className='footer-links'>
-          <div className="row">
-        {/* <h3 classNameName='footer-text' >2021 Yevgeniya Terlyuk</h3> */}
-        <div className="col-1-of-2" >
-        <div className='footer-linkedin'>
-<p><a href="https://www.linkedin.com/in/yevgeniya-terluyk-034013179/">linkdin</a></p>
-        </div>
-        </div>
-        <div className="col-1-of-2" >
-          <div className='footer-github'>
-                 <p><a href="https://github.com/down-dive">github</a></p>
-          </div>
-        </div>
-      </div>
+            <div className="row">
+              <div className="col-1-of-2" >
+                <div className='footer-github'>
+                  <SocialIcon url="https://linkedin.com/in/yevgeniya-terluyk-034013179/" bgColor="#fff" fgColor="#040605" />
+                </div>
+              </div>
+              <div className="col-1-of-2" >
+                <div className='footer-github'>
+                  <SocialIcon url="https://github.com/down-dive" bgColor="#fff" fgColor="#040605" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </footer>
-
-// import React from 'react'
-
-// export default () => {
-//   return (
-//     <div className='foot'>
-//       <p>website created by Thomas Gutierrez</p>
-
-//       <p><a href="https://www.linkedin.com/in/yevgeniya-terluyk-034013179/">linkdin</a> 
-//          <a href="https://github.com/down-dive">github</a></p>
-//     </div>
-//   )
-// }
-
-    //     <footer className="footer">
-    //     <div class="footer__logo-box">
-    //         <img src="img/logo-green-2x.png" alt="Full logo" class="footer__logo"></img>
-    //     </div>
-    //     <div class="row">
-    //         <div class="col-1-of-2">
-    //             <div class="footer__navigation">
-    //                 <ul class="footer__list">
-    //                     <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-    //                     <li class="footer__item"><a href="#" class="footer__link">Contact us</a></li>
-    //                     <li class="footer__item"><a href="#" class="footer__link">Carrers</a></li>
-    //                     <li class="footer__item"><a href="#" class="footer__link">Privacy policy</a></li>
-    //                     <li class="footer__item"><a href="#" class="footer__link">Terms</a></li>
-    //                 </ul>
-    //             </div>
-    //         </div>
-    //         <div class="col-1-of-2">
-    //             <p class="footer__copyright">
-    //                 Built by <a href="#" class="footer__link">Jonas Schmedtmann</a> for his online course <a href="#" class="footer__link">Advanced CSS and Sass</a>.
-    //                 Copyright &copy; by Jonas Schmedtmann. You are 100% allowed to use this webpage for both personal
-    //                 and commercial use, but NOT to claim it as your own design. A credit to the original author, Jonas
-    //                 Schmedtmann, is of course highly appreciated!
-    //             </p>
-    //         </div>
-    //     </div>
-    // </footer>
   );
 }
 export default Footer;
