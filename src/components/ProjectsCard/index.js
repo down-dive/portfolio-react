a555b0ca2b36e20575a54639a49525cf04d55c2c
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-01 17:19:16 -0700

    changing names for projects folders

---
+++ src/components/ProjectsCard/index.js
@@ -0,0 +1,138 @@
+// import React from 'react';
+// // import { makeStyles } from '@material-ui/core/styles';
+// // import Card from '@material-ui/core/Card';
+// // import CardActionArea from '@material-ui/core/CardActionArea';
+// // import CardContent from '@material-ui/core/CardContent';
+// // import CardMedia from '@material-ui/core/CardMedia';
+// // import Typography from '@material-ui/core/Typography';
+// // import './style.css'
+
+
+// // const useStyles = makeStyles({
+// //   root: {
+// //     maxWidth: 345,
+// //   },
+// //   media: {
+// //     height: 350,
+// //   },
+// // });
+// // const ProjectsCard = ({ name, github, deployedlink, tools, img}) => {
+// //   const classes = useStyles();
+// //   return (
+// //     <Card class="content-container" className={classes.root}>
+// //       <CardActionArea >
+// //         <CardMedia
+// //           className={classes.media}
+// //           image={img}
+// //           title={name}
+// //         />
+// //         <CardContent>
+// //           <Typography gutterBottom variant="h5" component="h2">
+// //             {name}
+// //           </Typography>
+// //           <Typography variant="body2" color="textSecondary" component="p">
+// //           <a href= {github}>Github</a>
+// //           </Typography>
+// //           <Typography variant="body2" color="textSecondary" component="p">
+// //           <a href= {deployedlink}>Deployed link</a>
+// //           </Typography>
+// //           <Typography variant="body2" color="textSecondary" component="p">
+// //             {tools}
+// //           </Typography>
+// //         </CardContent>
+// //       </CardActionArea>
+// //     </Card>
+// //   );
+// // }
+
+// // export default ProjectsCard
+
+// import projects from '../../projects.json'
+// // import image from {img}
+
+
+// export default function ProjectsCard({ name, github, deployedlink, tools, source , role}) {
+
+
+
+//   return (
+//     <div >
+//       {projects.map(project=> {
+//         return (
+//           <div class="card">
+//           <h4 class="card__heading">
+//             <span class="card__heading-span card__heading-span--1">{project.name}</span>
+//           </h4>
+//           <div class="card__details">
+//             <ul>
+//               <li><a className='link' href= {project.github}>Github</a></li>
+//               <li><a className='link' href= {project.deployedlink}>Deployed link</a></li>
+//               <li>{project.tools}</li>
+//               <li>{project.role}</li>
+//               {/* <li><img src={require(`../../assets/images/${name}.png`)}/></li> */}
+//               <li>    <div
+//       className='image'
+//       style={{
+//         backgroundImage: `url(${project.source})`
+//       }}
+//     /></li>
+//             </ul>
+//           </div>
+//       </div>
+//         )
+//       })
+
+// }
+//     </div>
+//   );
+
+// import image from '../../assets/images/book search.png'
+// export default function ProjectsCard({ name, github, deployedlink, tools, source, role }) {
+
+//   return (
+//     <div >
+//       <div class="card">
+//         <h4 class="card__heading">
+//           <span class="card__heading-span card__heading-span--1">{name}</span>
+//         </h4>
+//         <div class="card__details">
+//           <ul>
+//             <li><a className='link' href={github}>Github</a></li>
+//             <li><a className='link' href={deployedlink}>Deployed link</a></li>
+//             <li>{tools}</li>
+//             <li>{role}</li>
+//             <li><img className='image' src={image} /></li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+import image from '../../assets/images/book search.png'
+export default function ProjectsCard({ name, github, deployedlink, tools, source, role }) {
+
+  return (
+    <div >
+      <div className="card">
+<div className="col-1-of-2">
+          <img className="card__picture card__picture--1" src={image}/>
+          
+          </div>
+          <div className="col-1-of-2">
+          <h4 class="card__heading">
+            <span class="card__heading-span card__heading-span--1">{name}</span>
+          </h4>
+          <div class="card__details">
+            <ul>
+              <li><a className='link' href={github}>Github</a></li>
+              <li><a className='link' href={deployedlink}>Deployed link</a></li>
+              <li>{tools}</li>
+              <li>{role}</li>
+            </ul>
+          </div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
