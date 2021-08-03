a9dacb7c3212a5c8cea5c27fb816a86d0a2498fa
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-02 18:05:13 -0700

    contact information is displayed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
-
 const ContactForm = () => {
   return (
-    <section id="contact" className="section-book">
+    <section id="contact" className="section-contact">
       <div className="row">
-        <div className="book">
-          <div className="book__form">
-            <form action="#" className="form">
-              <div className="u-margin-bottom-medium">
-                <h2 className="heading-secondary">
-                  Contact Infomration
-                </h2>
-              </div>
-
-              <ul>
-                <li><strong>Email:</strong> <a  className='link' href="mailto:yterlyuk@gmail.com">yterlyuk@gmail.com</a></li>
-                <li><strong>Github:</strong> <a className='link' href="https://github.com/down-dive">down-dive</a></li>
-                <li>
-                  <strong>LinkedIn:</strong> <a className='link' href="www.linkedin.com/in/yevgeniya-terluyk-034013179">Yevgeniya Terlyuk</a>
-                </li>
-              </ul>
-            </form>
-          </div>
+        <div className="u-margin-bottom-medium">
+          <h2 className="heading-secondary">
+            Contact Infomration
+          </h2>
+        </div>
+        <div className='information'>
+          <ul>
+            <li><strong>Email:</strong> <a className='link' href="mailto:yterlyuk@gmail.com">yterlyuk@gmail.com</a></li>
+            <li><strong>Github:</strong> <a className='link' href="https://github.com/down-dive">down-dive</a></li>
+            <li>
+              <strong>LinkedIn:</strong> <a className='link' href="www.linkedin.com/in/yevgeniya-terluyk-034013179">Yevgeniya Terlyuk</a>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
